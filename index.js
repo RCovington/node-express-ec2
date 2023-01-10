c9542deeb494d70d2e03993f4de34b09db3042df
Russell Covington <rcovington@stone-ware.com>
2023-01-10 16:10:49 -0700

    second commit

--- index.js
+++ index.js
@@ -4,5 +4,24 @@ const app = express();
 
 app.listen(443, () => console.log('API running on 443'));
 
-app.get('/', (req, res) => res.json('My API running ...'));
+app.get('/', (req, res) => {
+    console.log("RUSS Req was: " + JSON.stringify(req.path));
+    res.json('My API running ...');
+});
+
+const favicon = new Buffer.from('AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA/4QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEREQAAAAAAEAAAEAAAAAEAAAABAAAAEAAAAAAQAAAQAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAEAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//wAA//8AAP//AAD8HwAA++8AAPf3AADv+wAA7/sAAP//AAD//wAA+98AAP//AAD//wAA//8AAP//AAD//wAA', 'base64'); 
+app.get("/favicon.ico", function(req, res) {
+ res.statusCode = 200;
+ res.setHeader('Content-Length', favicon.length);
+ res.setHeader('Content-Type', 'image/x-icon');
+ res.setHeader("Cache-Control", "public, max-age=2592000");                // expiers after a month
+ res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
+ res.end(favicon);
+});
+
+app.get('/*', (req, res) => {
+    console.log("RUSS Req was: " + JSON.stringify(req.path));
+    res.json('My *STAR* API running ...');
+});
+
 
